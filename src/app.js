50833d2d3ad12e9b776cd8b869e6acb4669d333c
dhrumil4u360@gmail.com <dhrumil4u360@gmail.com>
2022-01-26 10:19:57 +0530

    different event type based filtering

--- src/app.js
+++ src/app.js
@@ -16,6 +16,58 @@ async function webhookProcess(req, res, settings, triggerControllers) {
     }
 }
 
+async function runCreated(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.CREATED')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
+
+async function runSuccessed(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.SUCCEEDED')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
+
+async function runFailed(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.FAILED')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
+
+async function runAborted(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.ABORTED')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
+
+async function runTimedout(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.TIMED_OUT')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
+
+async function runResurrected(req, res, settings, triggerControllers) {
+    if(req.body.eventType == 'ACTOR.RUN.RESURRECTED')
+    {
+        return webhookProcess(req, res, settings, triggerControllers);
+    }
+}
+
 module.exports = {
-    webhookProcess
+    runCreated,
+    runAborted,
+    runFailed,
+    runResurrected,
+    runTimedout,
+    runSuccessed
 }
